c8d3e69870a45bc25fd34576a2983e3da1d90256
JackBrasesco <none>
2016-09-21 09:07:41 -0700

    Classadoo autocommit

--- game.js
+++ game.js
@@ -4,13 +4,13 @@ gameInput.keydown(function(keydownEvent) {
   // the key code for enter is 13
 
   if (keydownEvent.keyCode == 13) {  
-    var str = gameInput;
-    var find = str.indexOf("color");
-    console.log(find)
     parseText(gameInput.val()); 
     gameInput.val("");
   }  
 })
+var str = ("blah blah blah color blah");
+var find = str.indexOf("color");
+console.log(find)
 
 
 function parseText(text) {  
