33025c5305d7a685226af50dc063cacdeacefd60
JackBrasesco <jacbras@nuevaschool.org>
2016-10-04 09:20:14 -0700

    Classadoo autocommit

--- bob.js
+++ bob.js
@@ -40,6 +40,7 @@ gameInput.keydown(function(keydownEvent) {
     var ClaimFunction = str.indexOf("/claim")
     if (ClaimFunction > -1) {
       var thisvalue = str.split("/claim")[1]
+      
     if (GoogleFunction > -1) {
       var title = str.split("/google")[1];
       getWikiIntro(title, function(text) {
