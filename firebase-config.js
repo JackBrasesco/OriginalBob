5ad213c2ec0f2830e610f5c338709725b54c51d2
JackBrasesco <jacbras@nuevaschool.org>
2016-10-07 08:34:02 -0700

    Classadoo autocommit

--- firebase-config.js
+++ firebase-config.js
@@ -8,6 +8,15 @@ firebase.initializeApp(firebaseConfig);
 
 var store = firebase.database().ref("challenge-2")
 
+function addListItem(name, value) {		
+  store.child("vars" + "/" + name).push(value)
+}
+
+function onNewListItem(name, callback) {		
+  store.child("vars" + "/" + name).on("child_added", function(data) {
+    callback(data.val())
+  })
+}
 
 function storeValue(name, value) {	
   var obj = {}
