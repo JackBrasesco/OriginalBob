79e495fa6a64820dc949ff5f23c171cf1d87e763
JackBrasesco <jacbras@nuevaschool.org>
2016-10-09 20:32:02 -0700

    Classadoo autocommit

--- sign-in.js
+++ sign-in.js
@@ -10,28 +10,9 @@ var newUser = $("#suur");
 
 var newPassword = $("#supwone");
 
-var extUser = $("siun");
-
-var extPassword = $("sipw");
-
-// This is where accounts are created.
-
-$("#suenter").click(function(accountCreate) {
-  addListItem("UserDatabase",newUser);
-  addListItem("passwordDatabase",newPassword);
-  $(".sustuff").css({display: "none"})
-})
-
-// This is where you log in
-
-$("#sienter").click(function(accountSignIn) {
-  var userReal = extUser.indexOf(UserDatabase);
-  var passReal = extPassword.indexOf(passwordDatabase);
-  if (userReal > -1) {
-  }
-  if (passReal > -1) {
-  }
+$("#suenter").click(function(userEnter) {
+  addListItem("UserDatabase",n
+  
   
 })
-
-gameOutput.html(UserDatabase)
+  
