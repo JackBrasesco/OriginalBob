43187cf18c18abc91ca5050d0ea7cd8335967cdb
JackBrasesco <jacbras@nuevaschool.org>
2016-10-09 20:14:59 -0700

    Classadoo autocommit

--- sign-in.js
+++ sign-in.js
@@ -10,11 +10,16 @@ var newUser = $("#suur");
 
 var newPassword = $("#supwone");
 
+// This is where accounts are created.
+
 $("#suenter").click(function(accountCreate) {
   addListItem("UserDatabase",newUser);
   addListItem("passwordDatabase",newPassword);
   $(".sustuff").css({display: "none"})
 })
 
+// This is where you log in
+
+$("#
 
   
