9dbb8b3109a65b1cffe54ae797c280cf0414c49d
JackBrasesco <jacbras@nuevaschool.org>
2016-10-09 19:54:50 -0700

    Classadoo autocommit

--- sign-in.js
+++ sign-in.js
@@ -1,6 +1,8 @@
 $("#signin").click(function() {
   $(".siscreen").css({display: "inline"});
-  gameOutput.html("hi")
 })
 
-$("
+$("#signup").click(function() {
+  $(".sustuff").css({display: "inline"});
+})
+  
